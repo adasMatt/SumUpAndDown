3ca25f5f161bc89d024c3186e7852b7f54953b0c
Matt <madas@hawk.iit.edu>
2021-03-01 10:46:44 -0600

    Initial Commit

---
+++ Shared/SumUpAndDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  SumUpAndDownApp.swift
+//  Shared
+//
+//  Created by Matthew Adas on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SumUpAndDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
