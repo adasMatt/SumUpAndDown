0508f4ba2159bc3874a3207093027b63940be6c0
Matt <madas@hawk.iit.edu>
2021-03-01 12:08:52 -0600

    I have questions about what's being plotted

--- Shared/CalculatePlotData.swift
+++ Shared/CalculatePlotData.swift
@@ -62,51 +62,18 @@ class CalculatePlotData: ObservableObject {
         
     }
     
+    // is it supposed to be more like s1-s2/s2 and s1-s2/s1??
     func logUpMinusDownOverSumOfSums (num1: Double, num2: Double) -> Double {
         
-        let arithmetic = ((num1 - num2) / (num1 + num2))
+        
+        //let arithmetic = (abs(num1 - num2) / (num1 + num2))
+        let arithmetic = (abs(num1 - num2) / (num1))
         
         
         return log10(arithmetic)
     }
     
     
-    func ploteToTheMinusX()
-    {
-        
-        //set the Plot Parameters
-        plotDataModel!.changingPlotParameters.yMax = 10
-        plotDataModel!.changingPlotParameters.yMin = -3.0
-        plotDataModel!.changingPlotParameters.xMax = 10.0
-        plotDataModel!.changingPlotParameters.xMin = -3.0
-        plotDataModel!.changingPlotParameters.xLabel = "x"
-        plotDataModel!.changingPlotParameters.yLabel = "y = exp(-x)"
-        plotDataModel!.changingPlotParameters.lineColor = .blue()
-        plotDataModel!.changingPlotParameters.title = "exp(-x)"
-
-        plotDataModel!.zeroData()
-        var plotData :[plotDataType] =  []
-        for i in 0 ..< 60 {
-
-            //create x values here
-
-            let x = -2.0 + Double(i) * 0.2
-
-        //create y values here
-
-        let y = exp(-x)
-            
-            let dataPoint: plotDataType = [.X: x, .Y: y]
-            plotData.append(contentsOf: [dataPoint])
-            
-            plotDataModel!.calculatedText += "\(x)\t\(y)\n"
-            
-        }
-        
-        plotDataModel!.appendData(dataPoint: plotData)
-        
-        return
-    }
     
 }
 
